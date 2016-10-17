6df4791d643a08d82b6f29da0465ec62e761d955
dmcclow12861 <davidmcclow@hotmail.com>
2016-10-17 12:34:51 -0700

    Update emp_verification.cpp

--- emp_verification.cpp
+++ emp_verification.cpp
@@ -1,21 +1,14 @@
 #include "emp_verification.h"
 #include "ui_emp_verification.h"
 
-//stufffffff
 emp_verification::emp_verification(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::emp_verification)
 {
     ui->setupUi(this);
 
-    blockbusted_db = QSqlDatabase::addDatabase("QSQLITE");
-    blockbusted_db.setDatabaseName("C:/Users/Josh/Documents/BlockBusted/blockbusted_db.db");
+    DbManager blockbusted_db;
 
-    //Checks to see if database opened
-    if(!blockbusted_db.open())
-        ui->label->setText("Failed to connect to database...");
-    else
-        ui->label->setText("Connected to database...");
 }
 
 emp_verification::~emp_verification()
@@ -40,12 +33,6 @@ void emp_verification::on_signInButton_clicked()
     username = ui->lineEdit_Username->text();
     password = ui->lineEdit_Password->text();
 
-    if(!blockbusted_db.isOpen())
-    {
-        qDebug() << "Failed to connect...";
-        return;
-    }
-
     QSqlQuery qry;
 
     if(qry.exec("SELECT * FROM Employee WHERE EmpID = '"+ username +"' AND Password = '"+ password +"'"))
@@ -60,10 +47,14 @@ void emp_verification::on_signInButton_clicked()
         }
 
         if(count == 1 && managerFlag == 1)
+        {
             ui->label->setText("Signed in");
+            isSignedIn = true;
+            this->close();
+        }
         else if(count == 1 && managerFlag != 1)
             ui->label->setText("Insufficient privileges");
         else if(count < 1)
-            ui->label->setText("Incorrect");
+            ui->label->setText("Incorrect username or password");
     }
 }
